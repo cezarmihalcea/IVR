276fc4c84d83c3ef5d277b0abff269124e2c5c0a
Emma Hughes <s1802886@ed.ac.uk>
2019-11-21 16:56:28 +0000

    Controller.

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cezar/Projects/IVR/catkin_ws/src
-# Build directory: /home/cezar/Projects/IVR/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("ivr_assignment")
-subdirs("ivr_lab")
